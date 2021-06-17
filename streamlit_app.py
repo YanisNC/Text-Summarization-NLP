020fee1778f33b1929b3cc6155091cebe3d7fed2
YanisNC <yanisnc7@gmail.com>
2021-06-17 16:47:19 +0200

    modified .py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,18 @@ import re
 import time
 import nltk
 import gensim
+import sumy
 import validators
 
 from gensim.summarization import summarize as textrank_summarize
+
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+
+from sumy.summarizers.luhn import LuhnSummarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.summarizers.kl import KLSummarizer
 
 from nltk.tokenize import word_tokenize
 from transformers import pipeline
@@ -24,41 +33,51 @@ st.markdown('With this app you can summarize whatever you want!')
 
 customize = st.checkbox('Customize the settings')
 if customize:
-    model_name = st.selectbox('Select the model', ('BART', 'T5', 'PEGASUS'))
+    summary_type = st.selectbox('Select the type of summary', ('Extractive', 'Abstractive'))
+    if summary_type == 'Extractive':
+        extractive_name = st.selectbox('Select the algorithm', ('TextRank', 'Luhn', 'LexRank', 'LSA', 'KL-Sum'))
 
-    if model_name == 'BART':
-        _model = "facebook/bart-large-cnn"
-        _max_input_length = 512
-    elif model_name == 'T5':
-        _model = "t5-small"
-        _max_input_length = 512
-    elif model_name == 'PEGASUS':
-        sub_model_name = st.selectbox('Select the pre-trained model', ('Large', 'Bigbird', 'CNN / Dailymail', 'x-sum'))
-
-        if sub_model_name == 'Bigbird':
-            _model = "google/bigbird-pegasus-large-arxiv"
-            _max_input_length = 4096
-        elif sub_model_name == 'CNN / Dailymail':
-            _model = "google/pegasus-cnn_dailymail"
-            _max_input_length = 1024
-        elif sub_model_name == 'x-sum':
-            _model = "google/pegasus-xsum"
-            _max_input_length = 512
+        if extractive_name == 'TextRank':
+            _length_words = 200
+            _length_words = st.number_input("length (in words)", value=_length_words)
         else:
-            _model = "google/pegasus-large"
-            _max_input_length = 1024
+            _length_sentences = 5
+            _length_sentences = st.number_input("length (in sentences)", value=_length_sentences)
+    else:
+        model_name = st.selectbox('Select the model', ('BART', 'T5', 'PEGASUS'))
 
-    _min_length = 150
-    _max_length = 300
-    col1, col2 = st.beta_columns(2)
-    _min_length = col1.number_input("min_length", value=_min_length)
-    _max_length = col2.number_input("max_length", value=_max_length)
-else:
-    model_name = "PEGASUS"
-    _model = "google/pegasus-large"
-    _max_input_length = 1024
-    _min_length = 150
-    _max_length = 300
+        if model_name == 'BART':
+            _model = "facebook/bart-large-cnn"
+            _max_input_length = 512
+        elif model_name == 'T5':
+            _model = "t5-small"
+            _max_input_length = 512
+        elif model_name == 'PEGASUS':
+            sub_model_name = st.selectbox('Select the pre-trained model', ('Large', 'Bigbird', 'CNN / Dailymail', 'x-sum'))
+
+            if sub_model_name == 'Bigbird':
+                _model = "google/bigbird-pegasus-large-arxiv"
+                _max_input_length = 4096
+            elif sub_model_name == 'CNN / Dailymail':
+                _model = "google/pegasus-cnn_dailymail"
+                _max_input_length = 1024
+            elif sub_model_name == 'x-sum':
+                _model = "google/pegasus-xsum"
+                _max_input_length = 512
+            else:
+                _model = "google/pegasus-large"
+                _max_input_length = 1024
+
+        _min_length = 150
+        _max_length = 300
+        col1, col2 = st.beta_columns(2)
+        _min_length = col1.number_input("min_length (in words)", value=_min_length)
+        _max_length = col2.number_input("max_length (in words)", value=_max_length)
+
+else:  # default
+    summary_type = 'Extractive'
+    extractive_name = 'TextRank'
+    _length_words = 200
 
 text = st.text_area('Text Input or URL')
 
@@ -77,6 +96,15 @@ def scrap_web(link):
         article_text += p.text
 
     return article_text
+
+
+def scrap_web2(link):
+    scraped_data = urllib.request.urlopen(link)
+    article = scraped_data.read()
+    soup = bs.BeautifulSoup(article, 'html.parser')
+    for s in soup(['script', 'style']):
+        s.extract()
+    return (soup.text.strip()).encode('ascii', 'ignore').decode("utf-8")
 
 
 def preprocessing(article_text):
@@ -113,32 +141,60 @@ def print_time(seconds):
 
 def run_model(input_text):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if summary_type == 'Abstractive':
+        if model_name == "BART":
+            from transformers import BartTokenizer, BartForConditionalGeneration
+            model = BartForConditionalGeneration.from_pretrained(_model)
+            tokenizer = BartTokenizer.from_pretrained(_model)
 
-    if model_name == "BART":
-        from transformers import BartTokenizer, BartForConditionalGeneration
-        model = BartForConditionalGeneration.from_pretrained(_model)
-        tokenizer = BartTokenizer.from_pretrained(_model)
+        elif model_name == "T5":
+            from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-    elif model_name == "T5":
-        from transformers import T5Tokenizer, T5ForConditionalGeneration
+            model = T5ForConditionalGeneration.from_pretrained(_model)
+            tokenizer = T5Tokenizer.from_pretrained(_model)
 
-        model = T5ForConditionalGeneration.from_pretrained(_model)
-        tokenizer = T5Tokenizer.from_pretrained(_model)
+        elif model_name == "PEGASUS":
+            from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 
-    elif model_name == "PEGASUS":
-        from transformers import PegasusTokenizer, PegasusForConditionalGeneration
+            model = PegasusForConditionalGeneration.from_pretrained(_model)
+            tokenizer = PegasusTokenizer.from_pretrained(_model)
 
-        model = PegasusForConditionalGeneration.from_pretrained(_model)
-        tokenizer = PegasusTokenizer.from_pretrained(_model)
+        if len(input_text) > _max_input_length:
+            input_text = extractive_summary(input_text, _max_input_length, tokenizer)
 
-    if len(input_text) > _max_input_length:
-        input_text = extractive_summary(input_text, _max_input_length, tokenizer)
+        summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
+        output = summarizer(input_text, min_length=_min_length, max_length=_max_length)
 
-    summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
-    output = summarizer(input_text, min_length=_min_length, max_length=_max_length)
+        st.write('Summary')
+        st.success(output[0]['summary_text'])
 
-    st.write('Summary')
-    st.success(output[0]['summary_text'])
+    else:
+        if extractive_name == 'TextRank':
+            output = textrank_summarize(input_text, word_count=_length_words)
+        else:
+            # Initializing the parser
+            parser = PlaintextParser.from_string(input_text, Tokenizer('english'))
+
+            if extractive_name == 'Luhn':
+                summarizer = LuhnSummarizer()
+                output_list = summarizer(parser.document, sentences_count=_length_sentences)
+            elif extractive_name == 'Luhn':
+                summarizer = LexRankSummarizer()
+                output_list = summarizer(parser.document, sentences_count=_length_sentences)
+            elif extractive_name == 'Luhn':
+                summarizer = LsaSummarizer()
+                output_list = summarizer(parser.document, sentences_count=_length_sentences)
+            else:
+                summarizer = KLSummarizer()
+                output_list = summarizer(parser.document, sentences_count=_length_sentences)
+
+            sent_list = []
+            for sent in output_list:
+                sent_list.append(str(sent))
+            output = ' '.join(sent_list)
+
+        st.write('Summary')
+        st.success(output)
 
 
 if st.button('Submit'):
@@ -151,6 +207,9 @@ if st.button('Submit'):
         except urllib.error.URLError:
             st.warning("The URL is incorrect. Please try again.")
             st.stop()
+        # except ValueError:
+            # print("ValueError")
+            # text = scrap_web2(text)
 
     st.info("Your text is being summarized...")
 
